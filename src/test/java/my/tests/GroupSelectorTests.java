3f17418dfa69b959fdd446cf063fbacd9b5e417a
baubakg <development@gandomi.com>
2021-05-03 19:06:52 +0200

    Added javadoc

--- src/test/java/my/tests/GroupSelectorTests.java
+++ src/test/java/my/tests/GroupSelectorTests.java
@@ -40,6 +40,13 @@ public class GroupSelectorTests {
         ExternalGroupManager.setGroupDefinitions(new HashMap<String, String[]>());
     }
 
+    /**
+     * Here we select a normal method as a test. OK
+     *
+     * Author : gandomi
+     *
+     *
+     */
     @Test
     public void testNormalTestMethod() {
 
@@ -77,6 +84,13 @@ public class GroupSelectorTests {
                 is(equalTo(1)));
     }
 
+    /**
+     * Here we select a test class to be executed. OK
+     *
+     * Author : gandomi
+     *
+     *
+     */
     @Test
     public void testNormalTestClass() {
 
@@ -114,6 +128,17 @@ public class GroupSelectorTests {
                 is(equalTo(2)));
     }
 
+    /**
+     * Here we still select a Test Class to be executed, but the package
+     * contains a Nested Test class (both without previously test groups) -
+     * FAILS
+     * <p>
+     * The nested test class is also executed
+     *
+     * Author : gandomi
+     *
+     *
+     */
     @Test
     public void testNormalTestClass_packageIncludesNestedTestClass() {
 
@@ -151,6 +176,17 @@ public class GroupSelectorTests {
                 is(equalTo(2)));
     }
 
+    /**
+     * Here we still select a Nested Test Class to be executed, but the package
+     * contains a Normal Test class (both without previously test groups) -
+     * FAILS
+     * <p>
+     * The normal test class is also executed
+     * 
+     * Author : gandomi
+     *
+     *
+     */
     @Test
     public void testNestedTestClass_packageIncludesNormalTestClasses() {
 
@@ -191,6 +227,13 @@ public class GroupSelectorTests {
                 is(equalTo(2)));
     }
 
+    /**
+     * In this case the package only contains a nested test class. OK
+     *
+     * Author : gandomi
+     *
+     *
+     */
     @Test
     public void testNestedTestClass_allAlone() {
 
@@ -230,6 +273,14 @@ public class GroupSelectorTests {
                 is(equalTo(2)));
     }
 
+    /**
+     * In this case our package contains a nested test class, but also a normal
+     * est class. This time the test class already has groups. OK
+     *
+     * Author : gandomi
+     *
+     *
+     */
     @Test
     public void testNestedTestClass_withTestContainingGroup() {
 
@@ -270,44 +321,6 @@ public class GroupSelectorTests {
                 is(equalTo(2)));
     }
 
-    @Test
-    public void testNestedTestClass_idealSolution() {
-
-        ExternalGroupManager.addGroupDefinition("my.tests.data.nested.ClassContainer$NonGroupClass3",
-                "extnest");
-
-        // Rampup
-        TestNG myTestNG = createTestNG();
-        TestListenerAdapter tla = fetchTestResultsHandler(myTestNG);
-
-        // Define suites
-        XmlSuite mySuite = addSuitToTestNGTest(myTestNG, "Automated Suite External Group Checks Testing");
-
-        // Add listeners
-        mySuite.addListener("my.tests.ExternalGroupManager");
-
-        // Create an instance of XmlTest and assign a name for it.
-        XmlTest myTest = attachTestToSuite(mySuite, "Test Simple External Group Checks Tests");
-        myTest.addIncludedGroup("extnest");
-
-        //Define packages
-        List<XmlPackage> l_packages = new ArrayList<>();
-        l_packages.add(new XmlPackage("my.tests.data.*"));
-
-        myTest.setXmlPackages(l_packages);
-
-        myTestNG.run();
-
-        assertThat("We should have no failed methods", tla.getFailedTests().size(), is(equalTo(0)));
-
-        assertThat("We should have 2 succesdul tests", tla.getPassedTests().size(), is(equalTo(2)));
-
-        assertThat("We should have 1 successful methods",
-                tla.getPassedTests().stream()
-                        .filter(m -> m.getInstance().getClass().equals(NonGroupClass3.class))
-                        .collect(Collectors.toList()).size(),
-                is(equalTo(2)));
-    }
 
     //////////////////  Helpers
 
